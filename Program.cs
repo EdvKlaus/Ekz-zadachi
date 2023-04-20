1565f4e3dc461b29a23137997a3f074ecc3a0f87
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:49:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha20
+{
+    class Person
+    {
+        public string fullName;
+        public int age;
+
+        public Person()
+        {
+
+        }
+        public Person(string fullName, int age)
+        {
+            this.fullName = fullName;
+            this.age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("Егоров Д.Н.", 22);
+        }
+    }
+
+}
