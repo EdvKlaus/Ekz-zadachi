187c4e34a00f908426cee8f4d753015c7087c2ab
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:49:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Zadacha19
+{
+    class Person
+    {
+        public Person(string name, int age = 18)
+        {
+            Name = name;
+            if (age >= 0 && age <= 100)
+            {
+                Age = age;
+            }
+        }
+        public string Name;
+        private int Age;
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {Name}. Возраст: {Age}");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person("Ivan", 500);
+            person.Print();
+        }
+    }
+}
