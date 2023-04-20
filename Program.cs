6df8dba184aab03ee1f5191542e91e0222794428
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:32:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadacha3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 0;
+            Console.WriteLine("Введите n ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            // проверка отрицательности n
+            if (n < 0)
+            {
+                Console.WriteLine("n должен быть положительным");
+            }
+            else
+            {
+                Console.WriteLine("Введите x");
+                double x = Convert.ToDouble(Console.ReadLine());
+                for (var j = 2; j <= n; j++)
+                {
+                    double e = (Math.Pow((x), j)) / j;
+            
+                }
+                Console.WriteLine($"Ответ: {S}");
+            }
+        }
+    }
+}
