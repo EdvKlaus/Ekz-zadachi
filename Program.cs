833475ea0659885ced8d8079ed7b2246eebdaa27
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:41:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadacha11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(5, 155);
+
+            if (number <= 100 && number >= 25)
+            {
+                Console.WriteLine($"Число {number} входит в деопозон от 25 до 100");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не входит в деопозон от 25 до 100");
+            }
+        }
+    }
+}
