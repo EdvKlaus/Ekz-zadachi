dacc03e83ecc9ba3f7fd339dbbf55c211de906a7
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:38:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadacha9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число A");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            double a = Math.Abs(A - 10);
+            double b = Math.Abs(B - 10);
+
+            if(a < b)
+            {
+                Console.WriteLine($"Число: {A} ближе к 10 чем число {B}");
+            }
+            else if(a > b){
+                Console.WriteLine($"Число: {B} ближе к 10 чем число {A}");
+            }
+        }
+    }
+}
