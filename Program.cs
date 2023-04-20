72237f6c73c84d13b9aee17400ec87a888f1bd1b
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:54:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha24
+{
+    struct Student
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public int Group { get; set; }
+
+        public Student(string name, string surname, int group)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Group = group;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student[] students = new Student[3] { new Student("Дмитрий", "Лесов", 202013),
+                new Student("Димитрий", "Ласов", 202015),
+                new Student("Димутрий", "Ласеков", 202017) };
+        }
+    }
+}
