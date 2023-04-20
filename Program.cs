e9e216538954bfcf9ced17ff53ef08d518e166bb
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:50:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Zadacha21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string ip = Console.ReadLine();
+            Regex regex = new Regex("\\b((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){3}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\b");
+            MatchCollection matches = regex.Matches(ip);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Совпадений нет.");
+            }
+        }
+
+    }
+}
