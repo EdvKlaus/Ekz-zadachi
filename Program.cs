cb95827b12ff1a81739738b32e4e56f58d3d8c07
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:37:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadacha8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //Проверка на целостность
+            if (n % 1 == 0)
+            {
+                if ((n % 2) == 0)
+                {
+                    Console.WriteLine($"Число: {n} чётное");
+                }
+                else if ((n % 2) != 0)
+                {
+                    Console.WriteLine($"Число: {n} нечётное");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Число: {n} должно быть целым");
+            }
+        }
+    }
+}
