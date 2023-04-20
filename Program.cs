9acf820d090b286b997a7972d920e2b81976108c
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:43:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[3] {a, b, c };
+            Array.Sort(array);
+            a = array[0];
+            b = array[1];
+            c = array[2];
+
+            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+
+        }
+    }
+}
