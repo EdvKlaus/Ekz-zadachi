d1bd572a553b57a8f9bfe48a8504041bc24778a8
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:44:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zadacha14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double sum = 0;
+                while (true)
+                {
+                    Console.WriteLine("Введите число, если хотите остановить сложение введите stop");
+                    string value = Console.ReadLine();
+                    if (value == "stop")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        double number = Convert.ToDouble(value);
+                        sum += number;
+                    }
+                }
+                Console.WriteLine($"Сумма чисел = {sum}");
+            }
+            catch
+            {
+                Console.WriteLine("Введено не число и не stop программа завершена");
+            }
+        }
+    }
+}
