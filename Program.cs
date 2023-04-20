ada13cce9028ae6b4a638e58f5309702d5b8756f
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:52:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Zadacha22
+{
+    internal class Program
+    {
+        delegate void calcul(int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите чисто a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите чисто b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            void Plus(int A, int B)
+            {
+                int result = A + B;
+                Console.WriteLine($"A + B = {result}");
+            }
+            void Minus(int A, int B)
+            {
+                int result = A - B;
+                Console.WriteLine($"A - B = {result}");
+            }
+            void Ymnogit(int A, int B)
+            {
+                int result = A * B;
+                Console.WriteLine($"A * B = {result}");
+            }
+
+            calcul calculation = new calcul(Plus);
+            calculation += Minus;
+            calculation += Ymnogit;
+
+            calculation(a , b);
+        }
+    }
+}
