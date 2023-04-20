30b5e64340d95864d27c5b95012802af86048440
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-04-20 20:46:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_16
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x > 1) {
+                Console.WriteLine($"Факториал числа {x} = {Factorial(x)}");
+            }
+            else if(x < 1)
+            {
+                Console.WriteLine("Значение меньше 1");
+            }
+        }
+
+        public static int Factorial(int x)
+        { 
+            if (x == 1) return 1;
+            return Factorial(x - 1) * x;
+        }
+    }
+}
