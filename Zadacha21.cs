bb26002695613c18958a8f82264c8e2065d2e58a
EdvKlaus <131393806+EdvKlaus@users.noreply.github.com>
2023-05-15 15:13:19 +0300

    Update Zadacha21.cs

--- Zadacha21.cs
+++ Zadacha21.cs
@@ -12,21 +12,17 @@ namespace Zadacha21
         public static void Main()
         {
             string ip = Console.ReadLine();
-            Regex regex = new Regex("\\b((((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\.){3}(((25[0-5])|(2[0-4]\\d))|((1\\d{2})|(\\d{1,2})))\\b");
-            MatchCollection matches = regex.Matches(ip);
+            Regex regex = new Regex("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9]?)$");
+            Match  matches = regex.Match(ip);
 
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                {
-                    Console.WriteLine(match.ToString());
-                }
-            }
-            else
-            {
-                Console.WriteLine("Совпадений нет.");
-            }
-        }
+     if (matches.Success)
+     {
+     Console.WriteLine("IP: " + matches.Value);
+     }
+     else
+     {
+     Console.WriteLine("Совпадений нет.");
+     }
 
     }
 }
